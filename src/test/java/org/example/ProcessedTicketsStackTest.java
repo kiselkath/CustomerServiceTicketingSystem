a3aac3f0c3df2b470ec550351e3b79ad89023638
katekiselevich <kiselkath@gmail.com>
2025-05-05 15:23:40 +0200

    Fixed tests

--- src/test/java/org/example/ProcessedTicketsStackTest.java
+++ src/test/java/org/example/ProcessedTicketsStackTest.java
@@ -1,43 +1,47 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 /**
- * Unit tests for ProcessedTicketsStack
+ * Unit tests for ProcessedTicketsStack.
  */
+public class ProcessedTicketsStackTest {
 
-class ProcessedTicketsStackTest {
-
-    private ProcessedTicketsStack stack;
-    private Ticket ticket;
+    private  ProcessedTicketsStack stack ;
+    private   Ticket ticket;
 
     @BeforeEach
-    void setUp() {
+    void steUp(){
         stack = new ProcessedTicketsStack();
         ticket = new Ticket(1, "Issue with login");
+
+
     }
 
     @Test
-    void addAndPeekLastProcessedTicket() {
-        stack.addProcessedTicket(ticket);
-        assertEquals(1, stack.getProcessedTicketsCount);
+    public void testAddAndPeekProcessedTicket(){
 
-        Ticket ticket = stack.peekLastProcessesTicket();
+        stack.addProcessedTicket(ticket);
+        assertEquals(1, stack.getProcessedTicketsCount());
+
+        Ticket peeked = stack.peekLastProcessedTicket();
         assertEquals(ticket, peeked);
         assertEquals(1, stack.getProcessedTicketsCount());
+
+
     }
 
     @Test
     public void testRemoveLastProcessedTicket(){
         stack.addProcessedTicket(ticket);
-
         Ticket removed = stack.removeLastProcessedTicket();
         assertEquals(ticket, removed);
         assertEquals(0, stack.getProcessedTicketsCount());
     }
 
-}
+
 }
